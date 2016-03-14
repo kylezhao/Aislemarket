3cf17e000508d87b2e4c2e92316213ab888b64ea
PINGFANG ZHAO <P8ZHAO@uwaterloo.ca>
2016-03-14 10:56:55 -0400

    Adding new files

---
+++ Aislemarket/AMShoppingListDetailView.h
@@ -0,0 +1,13 @@
+//
+//  AMShoppingListDetailView.h
+//  Aislemarket
+//
+//  Created by Kyle Zhao on 2016-03-14.
+//  Copyright © 2016 Kyle Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMShoppingListDetailView : UITableViewController
+
+@end
